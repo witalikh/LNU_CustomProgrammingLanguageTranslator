7d34f074eb329a407196618d51c98d6eef875d76
Vitalii <witalikhomyn@gmail.com>
2025-04-06 02:16:09 +0200

    Changes in tokens

--- frontend/tokens.py
+++ frontend/tokens.py
@@ -3,6 +3,9 @@ Module containing token class and token types
 (some kind of enum) used in the frontend part of translator
 for building tokens during lexing and recognition when parsing AST tree.
 """
+from frontend.syntax import TokenType
+
+
 # NOTE for developing:
 # 1. use uppercase for constants (new token types)
 # 2. don't import anything except standard lib
@@ -10,57 +13,9 @@ for building tokens during lexing and recognition when parsing AST tree.
 # 4. Values of constants doesn't even matter, just be sure to not repeat them
 
 
-class TokenType:
-    END_OF_STATEMENT = "END_OF_STATEMENT"
-    COMMA = "COMMA"
-    COLON = "COLON"
-
-    BEGIN_OF_CODE = "BEGIN_CODE"
-    END_OF_CODE = "END_CODE"
-
-    BEGIN_OF_SCOPE = "BEGIN_OF_SCOPE"
-    END_OF_SCOPE = "END_OF_SCOPE"
-
-    OPENING_PARENTHESIS = "OPENING_BRACKET"
-    CLOSING_PARENTHESIS = "CLOSING_BRACKET"
-
-    OPENING_SQUARE_BRACKET = "OPENING_SQUARE_BRACKET"
-    CLOSING_SQUARE_BRACKET = "CLOSING_SQUARE_BRACKET"
-
-    DECIMAL_INTEGER_LITERAL = "DECIMAL_INTEGER_LITERAL"
-    HEXADECIMAL_INTEGER_LITERAL = "HEXADECIMAL_INTEGER_LITERAL"
-    OCTAL_INTEGER_LITERAL = "OCTAL_INTEGER_LITERAL"
-    BINARY_INTEGER_LITERAL = "BINARY_INTEGER_LITERAL"
-
-    FLOAT_LITERAL = "FLOAT_LITERAL"
-    IMAGINARY_FLOAT_LITERAL = "IMAGINARY_FLOAT_LITERAL"
-
-    STRING_LITERAL = "STRING_LITERAL"
-    CHAR_LITERAL = "CHAR_LITERAL"
-    # BYTE_LITERAL = "BYTE_LITERAL"
-    BYTE_STRING_LITERAL = "BYTE_STRING"
-    BOOLEAN_LITERAL = "BOOLEAN_LITERAL"
-    NULL_LITERAL = "NULL_LITERAL"
-    UNDEFINED_LITERAL = "UNDEFINED_LITERAL"
-
-    SIMPLE_TYPE = "SIMPLE_TYPE"
-    COMPOUND_TYPE = "COMPOUND_TYPE"
-    TYPE_MODIFIER = "TYPE_MODIFIER"
-    IDENTIFIER = "IDENTIFIER"
-
-    COMPARISON = "COMPARISON"
-    GENERIC_ASSIGNMENT = "GENERIC_ASSIGNMENT"
-    OPERATOR = "OPERATOR"
-    OPERAND = "OPERAND"
-
-    COMMENT = "COMMENT"
-
-    KEYWORD = "KEYWORDS"
-    CLASS_KEYWORD = "CLASS_KEYWORDS"
-
 
 class Token:
-    def __init__(self, token_type: str, token_value: str, line_number: int, column_position: int):
+    def __init__(self, token_type: TokenType, token_value: str, line_number: int, column_position: int):
         self._token_type = token_type
         self._token_value = token_value
 
@@ -68,10 +23,13 @@ class Token:
         self._column_position = column_position
 
     def __str__(self) -> str:
-        return f"{str(self._token_type)}, {self._token_value}"
+        return f"[{self._token_type.name}, {self._token_value}]"
+
+    def __repr__(self) -> str:
+        return f"[{self._token_type.name}, {self._token_value}]"
 
     @property
-    def type(self) -> str:
+    def type(self) -> "TokenType":
         return self._token_type
 
     @property
